b38bd5bc57233e7cf9e179f62ab9080a21dc6593
Joseph Rojo <jsimon.rojo@gmail.com>
2024-08-11 03:44:24 +0800

    Added plugins and keymaps

--- configs/opts.nix
+++ configs/opts.nix
@@ -1,21 +1,37 @@
 {
+	backup = false;
+	colorcolumn = "80";
+	cursorcolumn = true;
+	cursorline = true;
+	expandtab = true;        
+	foldmethod = "marker";
+	hidden = true;           
+	history = 100;           
+	hlsearch = true;
+	ignorecase = true;       
+	incsearch = true;
+	laststatus = 3;
+	lazyredraw = true;       
+	linebreak = true;        
 	number = true;
 	relativenumber = true;
-
-	hlsearch = true;
-	incsearch = true;
-
-	wrap = true;
-
+	scrolloff = 999;
+	shiftwidth = 2;
+	showmatch = true;
+	signcolumn = "yes:1";
+	smartcase = true;        
+	smartindent = true;      
+	softtabstop = 2;
+	splitbelow = true;       
+	splitright = true;       
 	swapfile = false;
-	backup = false;
-	undofile = true;
-
-	termguicolors = true;
-	scrolloff = 8;
-
-	cursorline = true;
-	cursorcolumn = true;
+	synmaxcol = 240;         
 	syntax = "on";
+	tabstop = 2;
+	termguicolors = true;
+	textwidth = 75;
+	undofile = true;
+	updatetime = 250;
+	wrap = false;
 }
 
